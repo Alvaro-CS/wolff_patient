ce8bd9408789bdddbe9c31997dee9418f5f47f38
Alvaro-CS <a.cascajero@usp.ceu.es>
2020-11-17 20:41:00 +0100

    Patient creation

---
+++ src/wolff_patient/Patient.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wolff_patient;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ALVARO
+ */
+public class Patient implements Runnable {
+private Date birthdate;
+private enum gender{MALE,FEMALE,OTHER};
+private String address;//ARRAY PENSAR
+private int SSNumber;
+private int telf;
+private String DNI;
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
