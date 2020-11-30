df9fb737e263ea11e218bfab08dbe68b18c42048
Alvaro-CS <a.cascajero@usp.ceu.es>
2020-11-30 19:27:35 +0100

    BitalinoMenu and LogIn separation

--- src/wolff_patient/BitalinoMenuController.java
+++ src/wolff_patient/BitalinoMenuController.java
@@ -13,61 +13,48 @@ package wolff_patient;
 
 import BITalino.BitalinoManager;
 import BITalino.Frame;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import java.sql.Connection;
-import java.sql.Statement;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
 
 public class BitalinoMenuController implements Initializable {
 
     BitalinoManager bitalinoManager = null;//Remove after clean
-//    @FXML
-//    private TextField userNameField;
-//    @FXML
-//    private PasswordField passwordField;
-//    @FXML
-//    private Button logInButton;
-//    @FXML
-//    private Button signUpButton;
-//    @FXML
-//    private Label loginMessageLabel;
-//
-//    public void loginButtonOnAction(ActionEvent event) {
-//        if (userNameField.getText().isEmpty() == false && passwordField.getText().isEmpty() == false) {
-//            validateLogin();
-//
-//        } else {
-//            //if Fields are empty
-//            loginMessageLabel.setText("Please enter ID and password");
-//        }
-//
-//    }
 
+    /**
+     *
+     * @param event
+     */
     @FXML
     public void connectBitalino(ActionEvent event) {
         bitalinoManager = new BitalinoManager("98:D3:C1:FD:2F:EC"); //El user tiene que meterlo
     }
-     @FXML
+
+    /**
+     *
+     * @param event
+     */
+    @FXML
     public void disconnectBitalino(ActionEvent event) {
         bitalinoManager.disconnect();
     }
+
+    /**
+     *
+     * @param event
+     */
     @FXML
 
     public void readECG(ActionEvent event) {
@@ -76,7 +63,16 @@ public class BitalinoMenuController implements Initializable {
     }
     @FXML
     Pane paneChart;
+
+    /**
+     * This method shows a line chart on-screen  with the ECG that has been 
+     * received. It adjusts max and min values of the chart. Line chart is shown 
+     * in a pane.
+     * @param event The event that triggers the ECG to appear
+     * 
+     */
     @FXML
+    
     public void showECG(ActionEvent event){
         XYChart.Series series = new XYChart.Series();
       //  series.setName("ECG data");
@@ -106,50 +102,58 @@ public class BitalinoMenuController implements Initializable {
         paneChart.getChildren().add(lineChart);
         System.out.println("Shown");
     }
-    
-//    @FXML
-//    public void validateLogin() {
+ 
+    /**
+     *This method creates a socket  for sending an ECG to a server. It gets the
+     * ECG data from "bitalinoManager" object, gets the useful information, that
+     * is located in analog[0] and writes the int[] object with all ECG values.
+     */
+    @FXML
+    public void sendECG(){
+         OutputStream outputStream = null;
+         ObjectOutputStream objectOutputStream = null;
+        System.out.println("Envia");
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 9000);
+            outputStream = socket.getOutputStream();
+        } catch (IOException ex) {
+            System.out.println("It was not possible to connect to the server.");
+            System.exit(-1);
+            Logger.getLogger(LogInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+             objectOutputStream = new ObjectOutputStream(outputStream);
+            //We get the data (Frame class) from Bitalino, get the useful info (int) and send it.
+            Frame[] frame=bitalinoManager.getFrame();
+                int[]ecg_data= new int[frame.length];
+             for (int i=0;i<frame.length;i++) {
+                 ecg_data[i]=frame[i].analog[0];
+             }
+             objectOutputStream.writeObject(ecg_data);
+            
+        } catch (IOException ex) {
+            System.out.println("Unable to write the object on the server.");
+            Logger.getLogger(LogInController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            releaseResources(outputStream, socket);
 
-        //This method checks if there's a user whith the ID and password
-        //  DBConnection connectNow= new dbConnection();
-        //  Connection connectdb= connectNow.getConnection();
-        // String verifyLogin="SELECT count(1) FROM user_account WHERE username=""+userNameField.getText()+" AND password=" " +passwordField.getText()+" "  "
-//        try {
-//            Statement statement= conectdb.createStatement();
-//            ResultSet queryResult=statement.executeQuery(verifyLogin);
-//            
-//            while(queryResult.next){
-//                if(queryResult.getInt(columnindex 1)==1){
-//                            loginMessageLabel.setText("Log in correct)";
-//                              createAccountForm(); no entiendo por que
-//
-//                    
-//                    
-//                }else{
-//                                 loginMessageLabel.setText("Invalid ID or password. Please try again");
-//
-//                    
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @FXML
-//    //opens registration form
-//    public void createAccountForm() {
-//        try {
-//            Parent root = FXMLLoader.load(getClass().getResource("RegistrationView.fxml"));
-//            Scene scene = new Scene(root);
-//            Stage registerStage = new Stage();
-//            registerStage.setScene(scene);
-//            registerStage.show();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+        }
+    }
+    
+    private static void releaseResources(OutputStream outputStream, Socket socket) {
+        try {
+            outputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LogInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LogInController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
