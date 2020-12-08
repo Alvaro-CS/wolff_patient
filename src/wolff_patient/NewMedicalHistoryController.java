26bc7eab85572684fe406535a6e8a8ca1628ee81
susan <susan@LAPTOP-68053J2C>
2020-12-08 19:18:42 +0100

    NewMedicalHistory GUI

---
+++ src/wolff_patient/NewMedicalHistoryController.java
@@ -0,0 +1,24 @@
+
+package wolff_patient;
+
+import POJOS.Clinical_record;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class NewMedicalHistoryController implements Initializable {
+
+
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       
+
+    }
+   
+    
+
+}
+
